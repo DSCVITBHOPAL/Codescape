7bc952935a6c2c336da06746dad082f0c5abdc01
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-04-10 09:18:41 +0530

    feat: add about section

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -7,7 +7,6 @@ import styled from 'styled-components';
 import rocket from './rocket.svg';
 
 const About = () => {
-  console.log('Change Indicator 1');
   return (
     <Section sectionColor={palette.yellow} sectionId="about">
       <img
@@ -15,39 +14,20 @@ const About = () => {
         style={{ display: 'block', margin: '-70px auto 20px auto' }}
         alt="About Icon"
       />
-      <MainHeader>WHAT IS IDEATE?</MainHeader>
+      <MainHeader>about?</MainHeader>
 
       <Paragraph>
-        <BodyHeader> Ideate | ˈīdēˌāt | vb.</BodyHeader>
         <BodyText style={{ marginTop: 20 }}>
-          To explore a wide variety of possible solutions through generating a
-          large quantity of diverse possible soutions, allowing you to step
-          beyond the obvious and explore a range of ideas.
+          A 4 hr long coding competition to prove you are the pro coder, organised as part of the most extraordinary event of the year Aditya on 23/April.
+          <BodyText style={{marginTop:20}}>
+            Develop your skill and buckle up to win exciting prizes.
+            </BodyText>
         </BodyText>
       </Paragraph>
 
       <Paragraph>
-        <BodyHeader>Ideate 2020 @ Duke is...</BodyHeader>
         <BodyText style={{ marginTop: 20 }}>
-          A two-day virtual designathon filled with awesome speakers, workshops,
-          and a design challenge to explore how design and innovation interact.
-          Ideate aims to observe how design thinking can be applied across
-          fields, and is open to design aficionados, curious newcomers, and
-          everyone in between!
-        </BodyText>
-      </Paragraph>
-
-      <Paragraph>
-        <BodyHeader>Our Purpose</BodyHeader>
-        <BodyText style={{ marginTop: 20 }}>
-          We hope that Ideate will inspire students from all backgrounds to
-          incorporate design thinking as part of their innovation process and to
-          use it as a tool in their endeavors.
-          As Duke's annual design conference, we’re here to show you
-          how design isn’t just about drawing or Adobe software: it enhances
-          technology by creating intuitive and meaningful experiences, it draws
-          on psychology to understand end-users, and it creates successful
-          businesses by driving human-centered development of product solutions.
+          We aim in fostering a sustainable coding culture on our campus.
         </BodyText>
       </Paragraph>
     </Section>
