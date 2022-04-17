9aef407789163fea70af9ab5fc3d63f79917f33f
Akshit1311 <a4748g@gmail.com>
2022-04-17 13:32:36 +0530

    Updates

---
+++ precache-manifest.bce00ce50b25d254ed1f542ec3839863.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda9979206fcc598c80f5fa66d9c2f82",
+    "url": "/Codescape/index.html"
+  },
+  {
+    "revision": "a0380ef6c4d6774caef1",
+    "url": "/Codescape/static/css/main.4c9ac43f.chunk.css"
+  },
+  {
+    "revision": "15bc6361a462a622f89f",
+    "url": "/Codescape/static/js/2.b7bf04f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Codescape/static/js/2.b7bf04f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0380ef6c4d6774caef1",
+    "url": "/Codescape/static/js/main.ca7bdacc.chunk.js"
+  },
+  {
+    "revision": "f0e8779633a8e87cdf5b",
+    "url": "/Codescape/static/js/runtime-main.ab44e060.js"
+  },
+  {
+    "revision": "001a477bbcd2f734c491833f2191c95a",
+    "url": "/Codescape/static/media/Apercu Bold.001a477b.woff"
+  },
+  {
+    "revision": "5eedd4d2cfc6ba3f88bc217ea28fdeaa",
+    "url": "/Codescape/static/media/Apercu Light.5eedd4d2.woff"
+  },
+  {
+    "revision": "73f151af934bff845bb88320e3e98ce8",
+    "url": "/Codescape/static/media/Apercu Medium.73f151af.woff"
+  },
+  {
+    "revision": "0b41c702be31f58f5d86ccd7c9108197",
+    "url": "/Codescape/static/media/Apercu Mono.0b41c702.woff"
+  },
+  {
+    "revision": "7f08dee7b13ac0050b5231c74d937ccd",
+    "url": "/Codescape/static/media/Apercu_Regular.7f08dee7.woff"
+  },
+  {
+    "revision": "5cee18ab599f3f5c2ea24d4916020454",
+    "url": "/Codescape/static/media/bust.5cee18ab.svg"
+  },
+  {
+    "revision": "0f492c2bedc4b9c9f76c43c3cf9982b1",
+    "url": "/Codescape/static/media/carolyn.0f492c2b.svg"
+  },
+  {
+    "revision": "7ae30a0e119db9329255f3e15702292a",
+    "url": "/Codescape/static/media/co-lab.7ae30a0e.svg"
+  },
+  {
+    "revision": "317f3af85a8ae89b15b05d3310c61552",
+    "url": "/Codescape/static/media/dorothy.317f3af8.svg"
+  },
+  {
+    "revision": "a46a6fa80986bba95d7003c7bc5b3e6b",
+    "url": "/Codescape/static/media/ece.a46a6fa8.svg"
+  },
+  {
+    "revision": "6d081cbe07b1679956983c08107e45a8",
+    "url": "/Codescape/static/media/email.6d081cbe.svg"
+  },
+  {
+    "revision": "de0bc401a56704513a6f5047e67488bf",
+    "url": "/Codescape/static/media/erin.de0bc401.svg"
+  },
+  {
+    "revision": "3a39df190d6c2dd771a50235ba3f48f0",
+    "url": "/Codescape/static/media/facebook.3a39df19.svg"
+  },
+  {
+    "revision": "997a6709853d09f36d05a7cfc00c3907",
+    "url": "/Codescape/static/media/faq-background.997a6709.svg"
+  },
+  {
+    "revision": "3b4509346df7870a4bf0a69b063fe229",
+    "url": "/Codescape/static/media/first.3b450934.svg"
+  },
+  {
+    "revision": "c5507bb7f5c11e2753f9a4cfde2f3b31",
+    "url": "/Codescape/static/media/footer-background.c5507bb7.svg"
+  },
+  {
+    "revision": "699d11193d747d55bdaafd9d86743da9",
+    "url": "/Codescape/static/media/fourth.699d1119.svg"
+  },
+  {
+    "revision": "0cbea7c11930e371ea65b6720c9291b0",
+    "url": "/Codescape/static/media/gdsc-logo.0cbea7c1.png"
+  },
+  {
+    "revision": "e3cd66fe1764fe9f7b2ef3fb00fee117",
+    "url": "/Codescape/static/media/jamal.e3cd66fe.svg"
+  },
+  {
+    "revision": "593a56c55fdf03acdf5adfda74879348",
+    "url": "/Codescape/static/media/kim.593a56c5.svg"
+  },
+  {
+    "revision": "c1f48c3046728ee45ef36a2e1db46b41",
+    "url": "/Codescape/static/media/kiyoon.c1f48c30.svg"
+  },
+  {
+    "revision": "83225d4491cb76548e506720bb1f0afe",
+    "url": "/Codescape/static/media/landing-background.83225d44.svg"
+  },
+  {
+    "revision": "ee3587be0499e02ed70fce6214e649a7",
+    "url": "/Codescape/static/media/left-sidebar.ee3587be.svg"
+  },
+  {
+    "revision": "13d58479c94b6cb844fbbea4a63716de",
+    "url": "/Codescape/static/media/linode.13d58479.svg"
+  },
+  {
+    "revision": "afdcc7f9e3bdf0bf0c9ea66739b5e7fb",
+    "url": "/Codescape/static/media/logo.afdcc7f9.svg"
+  },
+  {
+    "revision": "384c8bc680f9c8e849beb1a36581b898",
+    "url": "/Codescape/static/media/medium.384c8bc6.svg"
+  },
+  {
+    "revision": "97a1180c382667693c9faa5ceb2e27cc",
+    "url": "/Codescape/static/media/megan.97a1180c.svg"
+  },
+  {
+    "revision": "53c86651efe89629564f8269826238c5",
+    "url": "/Codescape/static/media/melvin.53c86651.svg"
+  },
+  {
+    "revision": "d592e481ef92aec4125ef2f1459b135f",
+    "url": "/Codescape/static/media/mems.d592e481.svg"
+  },
+  {
+    "revision": "039d6fcdcdaf206661e18de4e1557f00",
+    "url": "/Codescape/static/media/person.039d6fcd.png"
+  },
+  {
+    "revision": "896416f09dc6e47717245d8a966c1f93",
+    "url": "/Codescape/static/media/right-sidebar.896416f0.svg"
+  },
+  {
+    "revision": "91f218ce49dda12abc39febda15e0884",
+    "url": "/Codescape/static/media/rocket.91f218ce.svg"
+  },
+  {
+    "revision": "3e5aed05c296ee75b1a737f05b6c8d8d",
+    "url": "/Codescape/static/media/sanjana.3e5aed05.svg"
+  },
+  {
+    "revision": "75a0efd1db93c6f6d9d27a27927bbf5a",
+    "url": "/Codescape/static/media/second.75a0efd1.svg"
+  },
+  {
+    "revision": "5429a2ac0b8e6779dd5e6d521798da27",
+    "url": "/Codescape/static/media/shreya.5429a2ac.svg"
+  },
+  {
+    "revision": "09b629c65e2765a87dc186c9eae6792f",
+    "url": "/Codescape/static/media/shuya.09b629c6.svg"
+  },
+  {
+    "revision": "111054b5e92e38c060d8dbeb18e1dcb2",
+    "url": "/Codescape/static/media/sponsors-background.111054b5.svg"
+  },
+  {
+    "revision": "518faa567e917f6e3517d577bb13b3e5",
+    "url": "/Codescape/static/media/tania.518faa56.svg"
+  },
+  {
+    "revision": "7ade15a50e7497ee85a060aa2f659a07",
+    "url": "/Codescape/static/media/tastytrade.7ade15a5.svg"
+  },
+  {
+    "revision": "c31f7700cb3d8ed48665eb957eadc1e6",
+    "url": "/Codescape/static/media/third.c31f7700.svg"
+  },
+  {
+    "revision": "3d0b0a214fb545879816d0a0ab2d78b6",
+    "url": "/Codescape/static/media/tony.3d0b0a21.svg"
+  },
+  {
+    "revision": "53cbc25d6c77de29123d5667e04fefed",
+    "url": "/Codescape/static/media/whitney.53cbc25d.svg"
+  }
+]);
